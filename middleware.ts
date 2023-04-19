37f7197415785e57532a7e3e4a0f3e0919591102
camp-son <sjy2814@gmail.com>
2023-04-19 20:55:29 +0900

    fix: title

--- middleware.ts
+++ middleware.ts
@@ -40,13 +40,13 @@ const generateHTML = (title, description, imageUrl, url) => (
     <html>
         <head>
             <meta charset="utf-8">
-            <title>${title} | Sungkwang</title>
+            <title>${title} | CAMP-SON</title>
             <meta name="description" content="${description}">
             <meta property="og:url" content="${url}">
             <meta property="og:type" content="website">
-            <meta property="og:title" content="${title} | Sungkwang">
+            <meta property="og:title" content="${title} | CAMP-SON">
             <meta property="og:description" content="${description}">
-            <meta property="og:site_name" content="Sungkwang">
+            <meta property="og:site_name" content="CAMP-SON">
             <meta property="og:image" content="${imageUrl}">
             <meta name="twitter:card" content="summary">
             <meta name="twitter:url" content="${url}">
